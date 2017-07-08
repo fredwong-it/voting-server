ce50d3209c84d3b83e0b6727ee3910f94f50f536
Fred Wong <fredwong.it@gmail.com>
2017-07-08 14:49:09 -0400

    Broadcasting State from A Redux Listener

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
     const io = new Server().attach(8090);
+
+    // listen store and send a state snapshot whenever state changes
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS())
+    );
+
+    // listen to 'connection' events on our Socket.io server
+    // for clients to immediately receive the current state when they connect to the application
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+    });
 }
